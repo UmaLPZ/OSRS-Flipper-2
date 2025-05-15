bbf7a81a83e76c18432bd00301689490014dd19a
Juan Lopez <jlopezbnr@gmail.com>
2025-05-14 18:52:17 -0700

    -v1.0.5 -Switched to 3 separate columns instead of two for buy/sell/in progress tabs -Changed formatting for a lot of labels and values -Changed progress bars -Renamed some things -Added bottom line border

--- src/main/java/com/flipper2/views/inprogress/InProgressPanel.java
+++ src/main/java/com/flipper2/views/inprogress/InProgressPanel.java
@@ -1,5 +1,6 @@
 package com.flipper2.views.inprogress;
 
+import java.awt.Dimension;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 
@@ -10,6 +11,7 @@ import java.awt.image.BufferedImage;
 
 import com.flipper2.helpers.Numbers;
 import com.flipper2.helpers.UiUtilities;
+import com.flipper2.helpers.CustomPanel;
 import com.flipper2.views.components.InProgressHeader;
 
 import net.runelite.api.GrandExchangeOffer;
@@ -17,7 +19,10 @@ import net.runelite.api.GrandExchangeOfferState;
 import net.runelite.client.ui.ColorScheme;
 import net.runelite.api.ItemComposition;
 
+import static com.flipper2.helpers.GrandExchange.checkIsActive;
+import static com.flipper2.helpers.GrandExchange.checkIsBoughtSold;
 import static com.flipper2.helpers.GrandExchange.checkIsBuy;
+import static com.flipper2.helpers.GrandExchange.checkIsCancelState;
 
 public class InProgressPanel extends JPanel
 {
@@ -36,90 +41,108 @@ public class InProgressPanel extends JPanel
 
 		quantityProgressLabel = new JLabel();
 		quantityProgressLabel.setForeground(ColorScheme.GRAND_EXCHANGE_PRICE);
+		quantityProgressLabel.setBorder(new EmptyBorder(3, 0, 3, 0));
 		quantityProgressLabel.setHorizontalAlignment(JLabel.CENTER);
 
 		progressBar = new JProgressBar(0, 100);
 		progressBar.setBackground(ColorScheme.DARK_GRAY_COLOR);
-		progressBar.setForeground(UiUtilities.F2_GREEN);
+		progressBar.setForeground(ColorScheme.PROGRESS_INPROGRESS_COLOR);
 		progressBar.setStringPainted(true);
 
 		constructItemInfo();
 
 		container.setBorder(UiUtilities.ITEM_INFO_BORDER);
 		this.add(container, BorderLayout.NORTH);
-		this.setBorder(new EmptyBorder(0, 5, 3, 5));
+		this.setBorder(new EmptyBorder(3, 5, 3, 5));
 	}
 
 	private JLabel newLeftLabel(String text)
 	{
-		JLabel newJLabel = new JLabel(text);
-		newJLabel.setForeground(ColorScheme.GRAND_EXCHANGE_PRICE);
-		newJLabel.setBorder(new EmptyBorder(2, 2, 2, 2));
-		return newJLabel;
+		JLabel newLeftJLabel = new JLabel(text);
+		newLeftJLabel.setVerticalAlignment(JLabel.CENTER);
+		newLeftJLabel.setVerticalAlignment(JLabel.CENTER);
+		newLeftJLabel.setForeground(Color.white);
+		newLeftJLabel.setBorder(new EmptyBorder(4, 2, 3, 2));
+		return newLeftJLabel;
 	}
 
 	private JLabel newRightLabel(String value, Color fontColor)
 	{
 		JLabel newRightLabel = new JLabel(value);
-		newRightLabel.setHorizontalAlignment(JLabel.RIGHT);
+		newRightLabel.setHorizontalAlignment(JLabel.CENTER);
+		newRightLabel.setVerticalAlignment(JLabel.CENTER);
 		newRightLabel.setForeground(fontColor);
-		newRightLabel.setBorder(new EmptyBorder(2, 2, 2, 2));
+		newRightLabel.setBorder(new EmptyBorder(4, 2, 3, 2));
 		return newRightLabel;
 	}
 
 	private void constructItemInfo()
 	{
-		itemInfoContainer = new JPanel(new GridLayout(1, 2, 0, 0));
+		itemInfoContainer = new JPanel(new GridLayout(1, 3, 0, 0));
 		itemInfoContainer.setBackground(ColorScheme.DARK_GRAY_COLOR);
 
 		JPanel column1 = new JPanel(new BorderLayout());
 		column1.setBackground(ColorScheme.DARK_GRAY_COLOR);
 
-		JPanel contentPanel1 = new JPanel(new GridLayout(0, 1));
-		contentPanel1.setBackground(ColorScheme.DARK_GRAY_COLOR);
+		JPanel labelPanel = new JPanel(new GridLayout(0, 1));
+		labelPanel.setBackground(ColorScheme.DARK_GRAY_COLOR);
 
-		JPanel totalValuePanel = new JPanel(new BorderLayout());
-		totalValuePanel.setBackground(ColorScheme.DARK_GRAY_COLOR);
-		totalValuePanel.add(newLeftLabel("Offer Total:"), BorderLayout.WEST);
-		totalValuePanel.add(newRightLabel("0", ColorScheme.GRAND_EXCHANGE_ALCH), BorderLayout.EAST);
-		contentPanel1.add(totalValuePanel);
+		JPanel offerTotalPanelLabel = new CustomPanel(new BorderLayout(), true);
+		offerTotalPanelLabel.add(newLeftLabel("Offer Total:"), BorderLayout.WEST);
+		labelPanel.add(offerTotalPanelLabel);
 
-		JPanel pricePerPanel = new JPanel(new BorderLayout());
-		pricePerPanel.setBackground(ColorScheme.DARK_GRAY_COLOR);
-		pricePerPanel.add(newLeftLabel("Price/Per:"), BorderLayout.WEST);
-		pricePerPanel.add(newRightLabel("0", ColorScheme.GRAND_EXCHANGE_ALCH), BorderLayout.EAST);
-		contentPanel1.add(pricePerPanel);
+		JPanel pricePerPanelLabel = new CustomPanel(new BorderLayout(), true);
+		pricePerPanelLabel.add(newLeftLabel("Price/Per:"), BorderLayout.WEST);
+		labelPanel.add(pricePerPanelLabel);
 
-		column1.add(contentPanel1, BorderLayout.CENTER);
+		column1.add(labelPanel, BorderLayout.CENTER);
+
 
 		JPanel column2 = new JPanel(new BorderLayout());
 		column2.setBackground(ColorScheme.DARK_GRAY_COLOR);
 
+		JPanel contentPanel1 = new JPanel(new GridLayout(0, 1));
+		contentPanel1.setBackground(ColorScheme.DARK_GRAY_COLOR);
+
+		JPanel totalValuePanel = new CustomPanel(new BorderLayout(), true);
+		totalValuePanel.add(newRightLabel("0", ColorScheme.GRAND_EXCHANGE_ALCH));
+
+		contentPanel1.add(totalValuePanel);
+
+		JPanel pricePerPanel = new CustomPanel(new BorderLayout(), true);
+		pricePerPanel.add(newRightLabel("0", ColorScheme.GRAND_EXCHANGE_ALCH));
+		contentPanel1.add(pricePerPanel);
+
+		column2.add(contentPanel1, BorderLayout.CENTER);
+
+		JPanel column3 = new JPanel(new BorderLayout());
+		column3.setBackground(ColorScheme.DARK_GRAY_COLOR);
+
 		JPanel contentPanel2 = new JPanel(new GridLayout(0, 1));
 		contentPanel2.setBackground(ColorScheme.DARK_GRAY_COLOR);
 
-		JPanel spentPanel = new JPanel(new BorderLayout());
-		spentPanel.setBackground(ColorScheme.DARK_GRAY_COLOR);
-		spentPanel.add(newLeftLabel("Spent:"), BorderLayout.WEST);
-		spentPanel.add(newRightLabel("0", ColorScheme.GRAND_EXCHANGE_ALCH), BorderLayout.EAST);
+		JPanel spentPanel = new CustomPanel(new BorderLayout(), true);
+		spentPanel.add(newRightLabel("0", ColorScheme.GRAND_EXCHANGE_ALCH));
 		contentPanel2.add(spentPanel);
 
-		JPanel spentPerPanel = new JPanel(new BorderLayout());
-		spentPerPanel.setBackground(ColorScheme.DARK_GRAY_COLOR);
-		spentPerPanel.add(newLeftLabel("Spent/Per:"), BorderLayout.WEST);
-		spentPerPanel.add(newRightLabel("0", ColorScheme.GRAND_EXCHANGE_ALCH), BorderLayout.EAST);
+		JPanel spentPerPanel = new CustomPanel(new BorderLayout(), true);
+		spentPerPanel.add(newRightLabel("0", ColorScheme.GRAND_EXCHANGE_ALCH));
 		contentPanel2.add(spentPerPanel);
 
-		column2.add(contentPanel2, BorderLayout.CENTER);
+		column3.add(contentPanel2, BorderLayout.CENTER);
 
 		itemInfoContainer.add(column1);
 		itemInfoContainer.add(column2);
+		itemInfoContainer.add(column3);
 	}
 
 	public void updateOffer(ItemComposition offerItem, BufferedImage itemImage, GrandExchangeOffer offer)
 	{
 		GrandExchangeOfferState state = offer.getState();
 		boolean isBuy = checkIsBuy(state);
+		boolean isActive = checkIsActive(state);
+		boolean isBoughtSold = checkIsBoughtSold(state);
+		boolean isCancelled = checkIsCancelState(state);
 
 		if (container.getComponentCount() == 3)
 		{
@@ -130,13 +153,52 @@ public class InProgressPanel extends JPanel
 		InProgressHeader header = new InProgressHeader(offerItem, itemImage, offer);
 		container.add(header, BorderLayout.NORTH);
 
-		JPanel titlePanel = new JPanel(new GridLayout(1, 1, 0, 0));
+		JPanel titlePanel = new JPanel(new GridLayout(1, 3, 0, 0));
 		titlePanel.setBackground(ColorScheme.DARK_GRAY_COLOR);
-		String offerType = (isBuy ? "Buy" : "Sell");
+		titlePanel.setBorder(new EmptyBorder(2, 2, 2, 2));
+
+
+		String offerType;
+		Color offerColor;
+		if (isActive)
+		{
+			offerType = "Active";
+			offerColor = ColorScheme.PROGRESS_INPROGRESS_COLOR;
+		}
+		else if (isBoughtSold)
+		{
+			offerType = "Complete";
+			offerColor = ColorScheme.PROGRESS_COMPLETE_COLOR;
+		}
+		else if (isCancelled)
+		{
+			offerType = "Cancelled";
+			offerColor = ColorScheme.PROGRESS_ERROR_COLOR;
+		}
+		else
+		{
+			offerType = "Error";
+			offerColor = ColorScheme.PROGRESS_ERROR_COLOR;
+		}
+
 		JLabel offerTypeLabel = new JLabel(offerType);
-		offerTypeLabel.setHorizontalAlignment(JLabel.CENTER);
-		offerTypeLabel.setForeground(ColorScheme.GRAND_EXCHANGE_PRICE);
+		offerTypeLabel.setHorizontalAlignment(JLabel.LEFT);
+		offerTypeLabel.setForeground(offerColor);
+
+		String offerInit = ("Offered");
+		JLabel offerInitLabel = new JLabel(offerInit);
+		offerInitLabel.setHorizontalAlignment(JLabel.CENTER);
+		offerInitLabel.setForeground(Color.white);
+
+		String offerCurrent = (isBuy ? "Spent" : "Received");
+		JLabel offerCurrentLabel = new JLabel(offerCurrent);
+		offerCurrentLabel.setHorizontalAlignment(JLabel.CENTER);
+		offerCurrentLabel.setForeground(Color.white);
+
 		titlePanel.add(offerTypeLabel);
+		titlePanel.add(offerInitLabel);
+		titlePanel.add(offerCurrentLabel);
+
 
 		JPanel centerPanel = new JPanel(new BorderLayout());
 		centerPanel.setBackground(ColorScheme.DARK_GRAY_COLOR);
@@ -161,56 +223,56 @@ public class InProgressPanel extends JPanel
 		int percentage = totalQuantity > 0 ? (int) (((double) quantitySold / totalQuantity) * 100) : 0;
 		progressBar.setValue(percentage);
 		progressBar.setString(percentage + "%");
-		if (percentage >= 1 && percentage <= 24)
+		if (isActive && percentage <= 49)
 		{
-			progressBar.setForeground(UiUtilities.F2_ORANGE.brighter());
+			progressBar.setForeground(ColorScheme.PROGRESS_INPROGRESS_COLOR);
 		}
-		else if (percentage >= 25 && percentage <= 49)
+		else if (isActive && percentage >= 50 && percentage <= 99)
 		{
-			progressBar.setForeground(UiUtilities.F2_ORANGE);
+			progressBar.setForeground(ColorScheme.PROGRESS_COMPLETE_COLOR.brighter());
 		}
-		else if (percentage >= 50 && percentage <= 74)
+		else if (isBoughtSold && percentage == 100)
 		{
-			progressBar.setForeground(UiUtilities.F2_GREEN);
+			progressBar.setForeground(ColorScheme.PROGRESS_COMPLETE_COLOR);
 		}
-		else if (percentage >= 75 && percentage <= 100)
+		else if (isCancelled)
 		{
-			progressBar.setForeground(UiUtilities.F2_GREEN.brighter());
+			progressBar.setForeground(ColorScheme.PROGRESS_ERROR_COLOR);
 		}
 
 
-		JPanel contentPanel1 = (JPanel) ((JPanel) itemInfoContainer.getComponent(0)).getComponent(0);
-		JPanel contentPanel2 = (JPanel) ((JPanel) itemInfoContainer.getComponent(1)).getComponent(0);
+		JPanel contentPanel1 = (JPanel) ((JPanel) itemInfoContainer.getComponent(1)).getComponent(0);
+		JPanel contentPanel2 = (JPanel) ((JPanel) itemInfoContainer.getComponent(2)).getComponent(0);
 
-		JLabel totalValueValue = (JLabel) ((JPanel) contentPanel1.getComponent(0)).getComponent(1);
+		JLabel totalValueValue = (JLabel) ((JPanel) contentPanel1.getComponent(0)).getComponent(0);
 		int totalValue = totalQuantity * price;
 		totalValueValue.setText(Numbers.toShortNumber(totalValue));
 		totalValueValue.setToolTipText(Numbers.numberWithCommas(totalValue));
 
 
-		JLabel pricePerValue = (JLabel) ((JPanel) contentPanel1.getComponent(1)).getComponent(1);
+		JLabel pricePerValue = (JLabel) ((JPanel) contentPanel1.getComponent(1)).getComponent(0);
 		pricePerValue.setText(Numbers.toShortNumber(price));
 		pricePerValue.setToolTipText(Numbers.numberWithCommas(price));
 
-		String spentType = (isBuy ? "Spent:" : "Rcvd:");
-		JLabel spentTypeValue = (JLabel) ((JPanel) contentPanel2.getComponent(0)).getComponent(0);
-		spentTypeValue.setText(spentType);
 
-		JLabel spentValue = (JLabel) ((JPanel) contentPanel2.getComponent(0)).getComponent(1);
+		JLabel spentValue = (JLabel) ((JPanel) contentPanel2.getComponent(0)).getComponent(0);
 		spentValue.setText(Numbers.toShortNumber(spent));
 		spentValue.setToolTipText(Numbers.numberWithCommas(spent));
 
-		String spentTypePer = (isBuy ? "Spent/Per:" : "Rcvd/Per:");
-		JLabel spentTypePerValue = (JLabel) ((JPanel) contentPanel2.getComponent(1)).getComponent(0);
-		spentTypePerValue.setText(spentTypePer);
-
 		int spentPer = (quantitySold > 0) ? spent / quantitySold : 0;
-		JLabel spentPerValue = (JLabel) ((JPanel) contentPanel2.getComponent(1)).getComponent(1);
+		JLabel spentPerValue = (JLabel) ((JPanel) contentPanel2.getComponent(1)).getComponent(0);
 		spentPerValue.setText(Numbers.toShortNumber(spentPer));
 		spentPerValue.setToolTipText(Numbers.numberWithCommas(spentPer));
 
 		revalidate();
 		repaint();
+	}
+
+	@Override
+	public Dimension getPreferredSize()
+	{
+		Dimension preferredSize = super.getPreferredSize();
+		return new Dimension(container.getPreferredSize().width, preferredSize.height);
 	}
 
 	public void reset()
--- src/main/java/com/flipper2/views/transactions/TransactionPage.java
+++ src/main/java/com/flipper2/views/transactions/TransactionPage.java
@@ -12,43 +12,49 @@ import java.util.function.Consumer;
 
 import net.runelite.client.ui.ColorScheme;
 
-public class TransactionPage extends JPanel {
-    private JPanel container;
+public class TransactionPage extends JPanel
+{
+	private JPanel container;
 
-    public TransactionPage(Consumer<String> onSearchTextChanged) {
-        this.setLayout(new BorderLayout());
-        this.setBackground(ColorScheme.DARK_GRAY_COLOR);
-        this.addSearchText(onSearchTextChanged);
-        this.build();
-    }
+	public TransactionPage(Consumer<String> onSearchTextChanged)
+	{
+		this.setLayout(new BorderLayout());
+		this.setBackground(ColorScheme.DARK_GRAY_COLOR);
+		this.addSearchText(onSearchTextChanged);
+		this.build();
+	}
 
-    private void addSearchText(Consumer<String> onSearchTextChanged) {
-        SearchBar searchBar = new SearchBar(onSearchTextChanged);
-        this.add(searchBar, BorderLayout.NORTH);
-    }
+	private void addSearchText(Consumer<String> onSearchTextChanged)
+	{
+		SearchBar searchBar = new SearchBar(onSearchTextChanged);
+		this.add(searchBar, BorderLayout.NORTH);
+	}
 
-    public void resetContainer() {
-        container.removeAll();
-    }
+	public void resetContainer()
+	{
+		container.removeAll();
+	}
 
-    public void build() {
-        JPanel scrollContainer = new JPanel();
-        scrollContainer.setLayout(new BorderLayout());
-        container = new JPanel();
-        container.setBorder(new EmptyBorder(5, 0, 0, 0));
-        container.setLayout(new BoxLayout(container, BoxLayout.PAGE_AXIS));
-        container.setBackground(ColorScheme.DARK_GRAY_COLOR);
+	public void build()
+	{
+		JPanel scrollContainer = new JPanel();
+		scrollContainer.setLayout(new BorderLayout());
+		container = new JPanel();
+		container.setBorder(new EmptyBorder(5, 0, 0, 0));
+		container.setLayout(new BoxLayout(container, BoxLayout.PAGE_AXIS));
+		container.setBackground(ColorScheme.DARK_GRAY_COLOR);
 
-        JScrollPane scrollPane = new JScrollPane(scrollContainer);
-        scrollPane.setBackground(ColorScheme.LIGHT_GRAY_COLOR);
-        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+		JScrollPane scrollPane = new JScrollPane(scrollContainer);
+		scrollPane.setBackground(ColorScheme.LIGHT_GRAY_COLOR);
+		scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 
-        scrollContainer.add(container, BorderLayout.PAGE_START);
-        this.add(scrollPane, BorderLayout.CENTER);
-    }
+		scrollContainer.add(container, BorderLayout.PAGE_START);
+		this.add(scrollPane, BorderLayout.CENTER);
+	}
 
-    public void addTransactionPanel(TransactionPanel transactionPanel) {
-        container.add(transactionPanel, BorderLayout.CENTER);
-        this.revalidate();
-    }
+	public void addTransactionPanel(TransactionPanel transactionPanel)
+	{
+		container.add(transactionPanel, BorderLayout.CENTER);
+		this.revalidate();
+	}
 }
